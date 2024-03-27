8bb673e3f0c9c3538e61f3dd418da605b08f691f
cc0803 <simongeldbach@gmail.com>
2024-03-27 09:05:12 +0100

    feat: Add functionality to expand bucket size and fix some bugs

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ class HashObj {
 function hashMap() {
 	let buckets = Array(16);
 	let size = 16;
+	let elementAmount = 0;
 
-	const hash = (key) => hashFunction(key);
+	const hash = (key) => {
+		elementAmount += 1;
+		if (elementAmount >= size) {
+			size = size * 2;
+		}
+
+		return hashFunction(key);
+	};
 
 	const set = (key, value) => {
-		let index = Math.floor(hashFunction(key) % size);
+		let index = Math.floor(hash(key) % size);
 
 		// Check if index is in range of buckets
-		if (index < 0 || index >= buckets.length) {
+		if (index < 0 || index >= size) {
 			throw new Error("Trying to access index out of bound");
 		}
 
@@ -25,7 +33,7 @@ function hashMap() {
 		} else {
 			let current = buckets[index];
 
-			while (current.next != null) {
+			while (current != null) {
 				if (current.key == key) {
 					current.value = value;
 					break;
@@ -33,7 +41,7 @@ function hashMap() {
 				current = current.next;
 			}
 
-			current.next = new HashObj(key, value);
+			current = new HashObj(key, value);
 		}
 	};
 
@@ -42,7 +50,6 @@ function hashMap() {
 		let currentObj = buckets[index];
 
 		while (currentObj != null) {
-			console.log(currentObj);
 			if (key == currentObj.key) {
 				return currentObj.value;
 			}
@@ -53,7 +60,6 @@ function hashMap() {
 	};
 
 	const has = (key) => {
-		console.log(buckets);
 		let index = hash(key) % size;
 		let currentObj = buckets[index];
 
@@ -69,19 +75,21 @@ function hashMap() {
 		let currentObj = buckets[index];
 		let previousObj;
 
-		if (currentObj.key == key) {
-			buckets[index] = null;
-			return true;
-		}
-
-		while (currentObj != null) {
+		if (has(key)) {
 			if (currentObj.key == key) {
-				previousObj.next = currentObj.next;
+				buckets[index] = null;
 				return true;
 			}
 
-			previousObj = currentObj;
-			currentObj = currentObj.next;
+			while (currentObj != null) {
+				if (currentObj.key == key) {
+					previousObj.next = currentObj.next;
+					return true;
+				}
+
+				previousObj = currentObj;
+				currentObj = currentObj.next;
+			}
 		}
 
 		return false;
@@ -90,6 +98,7 @@ function hashMap() {
 	const length = () => {
 		let currentSize = 0;
 		let currentElement;
+		console.log(buckets);
 
 		for (let i = 0; i < size; i++) {
 			if (buckets[i] != null) {
@@ -189,7 +198,45 @@ myHashMap.set("Angelo", "Stiller");
 myHashMap.set("Michaela", "Michaelis");
 myHashMap.set("Patrick", "Seedorf");
 myHashMap.set("Marianne", "Sürbrock");
+myHashMap.set("Lili", "Amtor");
+myHashMap.set("Frieda", "Pablos");
+myHashMap.set("Erwin", "Müller");
+myHashMap.set("Dora", "McBeth");
+myHashMap.set("Siziliano", "Zubemendi");
+myHashMap.set("Xavier", "Bobles");
+myHashMap.set("Cloey", "Humphrey");
+myHashMap.set("Millisaint", "Virginia");
+myHashMap.set("Florence", "Georgia");
+myHashMap.set("Bellatrix", "Grey");
+myHashMap.set("James", "Maximov");
+myHashMap.set("Konstanze", "Lombok");
 
-console.log(myHashMap.keys());
-console.log(myHashMap.values());
-console.log(myHashMap.entries());
+myHashMap.set("Leon", "Leonson");
+myHashMap.set("Isabel", "Hut");
+myHashMap.set("Karin", "Stiefel");
+myHashMap.set("Lera", "Stempel");
+myHashMap.set("Mark", "Frerichs");
+myHashMap.set("Joshua", "Norden");
+myHashMap.set("Frank", "Walter");
+myHashMap.set("Angelo", "Stiller");
+myHashMap.set("Michaela", "Michaelis");
+myHashMap.set("Patrick", "Seedorf");
+myHashMap.set("Marianne", "Sürbrock");
+myHashMap.set("Lili", "Amtor");
+myHashMap.set("Frieda", "Pablos");
+myHashMap.set("Erwin", "Müller");
+myHashMap.set("Dora", "McBeth");
+myHashMap.set("Siziliano", "Zubemendi");
+myHashMap.set("Xavier", "Bobles");
+myHashMap.set("Cloey", "Humphrey");
+myHashMap.set("Millisaint", "Virginia");
+myHashMap.set("Florence", "Georgia");
+myHashMap.set("Bellatrix", "Grey");
+myHashMap.set("James", "Maximov");
+myHashMap.set("Konstanze", "Lombo");
+
+console.log(myHashMap.length());
+
+// console.log(myHashMap.keys());
+// console.log(myHashMap.values());
+// console.log(myHashMap.entries());
